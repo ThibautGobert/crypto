334a1b190b6e0591f22a8918365c1bf818e600e5
Thibaut <tg@bms-engineering.lu>
2025-02-08 11:01:34 +0100

    absolute

--- resources/js/Pages/CryptoTrade.vue
+++ resources/js/Pages/CryptoTrade.vue
@@ -209,7 +209,7 @@ onMounted(async () => {
                     :duration="200"
                 />
             </div>
-            <current-date-time style="color: white;position: absolute; right: 0; bottom: 0;"></current-date-time>
+            <current-date-time style="color: white;position: absolute; right: 15px; bottom: -30px;"></current-date-time>
         </div>
 
         <canvas ref="chartCanvas" style="width: 100%; height: 100%"></canvas>
