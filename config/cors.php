1e618cbc4dd4d44b3b15573fbfc3b092e86156b4
Thibaut <tg@bms-engineering.lu>
2025-02-15 16:37:50 +0100

    test

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'crypto-trade/candles'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'crypto/candles'],
 
     'allowed_methods' => ['*'],
 
